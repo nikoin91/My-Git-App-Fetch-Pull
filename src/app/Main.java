12ad991540447bf7495975461eee378dd39a8c20
Nikolaev Igor <nikolaevin91@gmail.com>
2024-10-26 22:24:10 +0300

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculator app.");
+        System.out.println("My git app. Super!");
+        System.out.println("Version 1.0");
     }
 }
